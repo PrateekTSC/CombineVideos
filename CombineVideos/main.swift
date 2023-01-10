eeefcd00b6fb9f2f2dda27ad1a3d700079739a19
PrateekTSC <p.prakash@techsmith.com>
2023-01-10 11:24:53 -0500

    Boilerplate Code For Transforms

--- CombineVideos/main.swift
+++ CombineVideos/main.swift
@@ -12,16 +12,23 @@ let firstAssetUrl = URL(fileURLWithPath: "/Users/p.prakash/Downloads/Video-1.mp4
 let secondAssetUrl = URL(fileURLWithPath: "/Users/p.prakash/Downloads/Video-2.mp4")
 let thirdAssetUrl = URL(fileURLWithPath: "/Users/p.prakash/Downloads/Video-3.mp4")
 
-let allAssets = [AVURLAsset(url: firstAssetUrl), AVURLAsset(url: secondAssetUrl), AVURLAsset(url: thirdAssetUrl)]
+let allAssets = [AVURLAsset(url: firstAssetUrl), AVURLAsset(url: secondAssetUrl)]
 
 let avComposition = AVMutableComposition()
 var insertTime = CMTime.zero
+var totalDuration = CMTime.zero
 
 let videoTrack = avComposition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
 let audioTrack = avComposition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
 
 var maxWidth = CGFloat.zero
 var maxHeight = CGFloat.zero
+
+let videoComposition = AVMutableVideoComposition()
+let renderSize = CGSize(width: 1600, height: 900)
+videoComposition.renderSize = renderSize
+videoComposition.frameDuration = CMTime(value: 1, timescale: 30)
+var layerInstructions: [AVVideoCompositionLayerInstruction] = []
 
 for currAsset in allAssets {
    do {
@@ -40,6 +47,13 @@ for currAsset in allAssets {
       try videoTrack?.insertTimeRange(assetRange, of: assetVideo, at: insertTime)
       try audioTrack?.insertTimeRange(assetRange, of: assetAudio, at: insertTime)
       
+      let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: assetVideo)
+      let preferredTransform = try await assetVideo.load(.preferredTransform)
+      let newTransform = preferredTransform.scaledBy(x: 1.0, y: 1.0).translatedBy(x: 0.0, y: 0.0)
+      layerInstruction.setTransform(newTransform, at: .zero)
+      layerInstructions += [layerInstruction]
+      
+      // insertTime == totalDuration
       insertTime = CMTimeAdd(insertTime, assetDuration)
    } catch {
       debugPrint(error)
@@ -48,10 +62,17 @@ for currAsset in allAssets {
 
 print("Max Video Size: \(maxWidth)x\(maxHeight)")
 
+let videoCompositionInstruction = AVMutableVideoCompositionInstruction()
+videoCompositionInstruction.layerInstructions = layerInstructions
+// insertTime == totalDuration
+videoCompositionInstruction.timeRange = CMTimeRange(start: .zero, duration: insertTime)
+videoComposition.instructions = [videoCompositionInstruction]
+
 guard let exportSession = AVAssetExportSession(asset: avComposition, presetName: AVAssetExportPresetHighestQuality) else {
    debugPrint("exportSession Error")
    exit(-1)
 }
+exportSession.videoComposition = videoComposition
 exportSession.outputURL = URL(fileURLWithPath: "/Users/p.prakash/Downloads/Combined.mp4")
 exportSession.outputFileType = AVFileType.mp4
 await exportSession.export()
